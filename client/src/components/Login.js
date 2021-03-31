7066fb141fd31075d3ee5154bc3c4b59248eb33c
j.janin-reynaud <julien.janin-reynaud@itsfactory.fr>
2021-03-31 19:08:02 +0200

    Splitted Settings

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -61,7 +61,7 @@ const Login = (props) => {
   };
 
   return (
-    <div className="col-md-12">
+    <div className="col-md-12" id={"login"}>
       <div className="card card-container">
         <img
           src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
--- client/src/components/TopMenu.js
+++ client/src/components/TopMenu.js
@@ -17,38 +17,31 @@ const TopMenu = () => {
             <Link to={"/"} className="navbar-brand">
             AutoMatch
             </Link>
-            <div className="navbar-nav mr-auto">
-                <li className="nav-item">
-                    <Link to={"/home"} className="nav-link">Home</Link>
-                </li>
 
-                {currentUser && (
-                    <li className="nav-item">
-                        <Link to={"/user"} className="nav-link">User</Link>
-                    </li>
+            <div className="navbar-nav ml-auto">
+                {currentUser ? (
+                    <>
+                        <li className="nav-item">
+                            <Link to={"/home"} className="nav-link">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/user"} className="nav-link">Settings</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/login"} className="nav-link" onClick={logOut}>LogOut</Link>
+                        </li>
+                    </>
+                ) : (
+                    <>
+                        <li className="nav-item">
+                            <Link to={"/login"} className="nav-link">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to={"/register"} className="nav-link">Sign Up</Link>
+                        </li>
+                    </>
                 )}
             </div>
-
-            {currentUser ? (
-            <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link to={"/profile"} className="nav-link">{currentUser.username}</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to={"/login"} className="nav-link" onClick={logOut}>LogOut</Link>
-                </li>
-            </div>
-            ) : (
-            <div className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <Link to={"/login"} className="nav-link">Login</Link>
-                </li>
-
-                <li className="nav-item">
-                    <Link to={"/register"} className="nav-link">Sign Up</Link>
-                </li>
-            </div>
-            )}
         </nav>
     );
 };
